c0cf5b27286db6b1fa51f6c8a269a983cdb1dd57
Salazar <praveenbaghel88990@gmail.com>
2023-09-06 13:35:58 +0530

    updated resume and some projects

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,6 +1,6 @@
 import { Container } from "./styles";
 
-import profileImage from "../../assets/profile-image.jpg"
+import profileImage from "../../assets/profile-image.jpg";
 import cssIcon from "../../assets/css-icon.svg";
 import htmlIcon from "../../assets/html-icon.svg";
 import jsIcon from "../../assets/js-icon.svg";
@@ -10,31 +10,45 @@ import flutterIcon from "../../assets/icons8-flutter.svg";
 import mongoIcon from "../../assets/icons8-mongodb.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
-
-export function About(){
-  return(
+export function About() {
+  return (
     <Container id="sobre">
       <div className="about-text">
         <ScrollAnimation animateIn="fadeInLeft">
           <h2>About me</h2>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.2 * 1000}>
-          <p>My name is Praveen Baghel, I am a computer science student at GLA university currently in 3rd year. I am passionate about delivering solutions that aggregate in people's lives and at the same time challenge me. Improve my skills as a Front-End and Back-End developer</p>
+          <p>
+            My name is Praveen Baghel, I am a computer science student at GLA
+            university currently in 4th year. I am passionate about delivering
+            solutions that aggregate in people's lives and at the same time
+            challenge me. Improve my skills as a Front-End and Back-End
+            developer
+          </p>
         </ScrollAnimation>
-        <ScrollAnimation animateIn="fadeInLeft" delay={0.4 * 1000} style={{marginTop: "2rem", marginBottom: "2rem"}}>
-
-          <p>I develop websites and applications using HTML, CSS, and JavaScript. I am familiar with developing layouts that provide me. I am always improving myself with every project they put in my hands.</p>
+        <ScrollAnimation
+          animateIn="fadeInLeft"
+          delay={0.4 * 1000}
+          style={{ marginTop: "2rem", marginBottom: "2rem" }}
+        >
+          <p>
+            I develop websites and applications using ReactJS. I am familiar with
+            developing layouts that provide me. I am always improving myself
+            with every project they put in my hands.
+          </p>
         </ScrollAnimation>
         <ScrollAnimation animateIn="fadeInLeft" delay={0.6 * 1000}>
-
-          <p>I am a dedicated person who runs behind to fulfill his dreams, worker and results-oriented, I always seek to achieve my best version.</p>
+          <p>
+            I am a dedicated person who runs behind to fulfill his dreams,
+            worker and results-oriented, I always seek to achieve my best
+            version.
+          </p>
         </ScrollAnimation>
 
         <ScrollAnimation animateIn="fadeInLeft" delay={0.7 * 1000}>
           <h3>Here are my main skills:</h3>
         </ScrollAnimation>
         <div className="hard-skills">
-
           <div className="hability">
             <ScrollAnimation animateIn="fadeInUp" delay={0.1 * 1000}>
               <img src={reactIcon} alt="React" />
@@ -42,43 +56,40 @@ export function About(){
           </div>
 
           <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}> 
-            <img src={jsIcon} alt="JavaScript" />
-          </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.3 * 1000}>
+              <img src={jsIcon} alt="JavaScript" />
+            </ScrollAnimation>
           </div>
 
           <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}> 
-            <img src={htmlIcon} alt="Html" />
-          </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.4 * 1000}>
+              <img src={htmlIcon} alt="Html" />
+            </ScrollAnimation>
           </div>
 
           <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}> 
-            <img src={cssIcon} alt="Css" />
-          </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.5 * 1000}>
+              <img src={cssIcon} alt="Css" />
+            </ScrollAnimation>
           </div>
           <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}> 
-            <img src={nodeIcon} alt="Node" />
-          </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.6 * 1000}>
+              <img src={nodeIcon} alt="Node" />
+            </ScrollAnimation>
           </div>
 
           <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
-            <img src={flutterIcon} alt="Typescript" />
-          </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
+              <img src={flutterIcon} alt="Typescript" />
+            </ScrollAnimation>
           </div>
           <div className="hability">
-          <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
-            <img src={mongoIcon} alt="Typescript" />
-          </ScrollAnimation>
+            <ScrollAnimation animateIn="fadeInUp" delay={0.7 * 1000}>
+              <img src={mongoIcon} alt="Typescript" />
+            </ScrollAnimation>
           </div>
-
-
         </div>
       </div>
-      
     </Container>
-  )
+  );
 }
--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,53 +1,48 @@
 import { Container } from "./styles";
-import githubIcon from "../../assets/github-icon.svg"
-import externalLinkIcon from "../../assets/external-link-icon.svg"
+import githubIcon from "../../assets/github-icon.svg";
+import externalLinkIcon from "../../assets/external-link-icon.svg";
 import ScrollAnimation from "react-animate-on-scroll";
 
-
-export function Portfolio(){
-  return(
+export function Portfolio() {
+  return (
     <Container id="portfolio">
       <h2>Projects..</h2>
 
       <div className="projects">
-
-      <ScrollAnimation animateIn="flipInX">
-        <div className="project">
-          <header>
-          <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-          <div className="project-links">
-            {/* <a href="https://github.com/cybergaz/Hyprland_Rice" target="_blank" rel="noreferrer">
-              <img src={githubIcon} alt="GitHub" />
-            </a> */}
-          </div>
-          </header>
-          <div className="body">
-            <h3>Hyprland_Rice</h3>
-            <p> It's my personal Linux Desktop Environment setup that i update over time depending on my usage. it is based on Arch Linux and uses hyprland as the window manager. 
-            </p>
-          </div>
-          <footer>
-            <ul className="tech-list">
-              <li>ArchLinux</li>
-              <li>Shell</li>
-            </ul>
-          </footer>
-        </div>
-      </ScrollAnimation>
-
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              {/* <a href="https://github.com/cybergaz/scripts/tree/main/wofi-emoji" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="GitHub" />
-              </a> */}
-            </div>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/Praveen-baghel/Guess_What"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="GitHub" />
+                </a>
+              </div>
             </header>
             <div className="body">
               <h3>Guess What?</h3>
-              <p> A flutter based multiplayer game that allows multiple users to make seperate rooms using Socket Programming and play a draw and guess game.
+              <p>
+                {" "}
+                A flutter based multiplayer game that allows multiple users to
+                make seperate rooms using Socket Programming and play a draw and
+                guess game.
               </p>
             </div>
             <footer>
@@ -64,16 +59,73 @@ export function Portfolio(){
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              {/* <a href="https://github.com/cybergaz/Kaoi" target="_blank" rel="noreferrer">
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                <a
+                  href="https://github.com/Praveen-baghel/ExchangeZone"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img src={githubIcon} alt="Visitar site" />
+                </a>
+              </div>
+            </header>
+            <div className="body">
+              <h3>Exchange Zone</h3>
+              <p>An online platform to buy and sell anything.</p>
+            </div>
+            <footer>
+              <ul className="tech-list">
+                <li>ReactJS</li>
+                <li>Firebase Firestore</li>
+                <li>Firebase Storage</li>
+              </ul>
+            </footer>
+          </div>
+        </ScrollAnimation>
+
+        <ScrollAnimation animateIn="flipInX">
+          <div className="project">
+            <header>
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                {/* <a href="https://github.com/cybergaz/Kaoi" target="_blank" rel="noreferrer">
               <img src={githubIcon} alt="GitHub" />
               </a> */}
-            </div>
+              </div>
             </header>
             <div className="body">
               <h3>Smart Water Management</h3>
-              <p> An IoT based project implementing water system in industries using Iot,cloud and automation.
+              <p>
+                {" "}
+                An IoT based project implementing water system in industries
+                using Iot,cloud and automation.
               </p>
             </div>
             <footer>
@@ -88,16 +140,31 @@ export function Portfolio(){
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              {/* <a href="https://github.com/cybergaz/heavens_anime" target="_blank" rel="noreferrer">
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                {/* <a href="https://github.com/cybergaz/heavens_anime" target="_blank" rel="noreferrer">
               <img src={githubIcon} alt="GitHub" />
               </a> */}
-            </div>
+              </div>
             </header>
             <div className="body">
               <h3>Smart Electric Vehicle</h3>
-              <p>An IoT based project implementing Modern Electric Vehicle in industries using IoT,cloud, geofencing etc.
+              <p>
+                An IoT based project implementing Modern Electric Vehicle in
+                industries using IoT,cloud, geofencing etc.
               </p>
             </div>
             <footer>
@@ -112,16 +179,31 @@ export function Portfolio(){
         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              {/* <a href="https://github.com/cybergaz/To_Do_List" target="_blank" rel="noreferrer">
+              <svg
+                width="50"
+                xmlns="http://www.w3.org/2000/svg"
+                role="img"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="#23ce6b "
+                stroke-width="1"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <title>Folder</title>
+                <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
+              </svg>
+              <div className="project-links">
+                {/* <a href="https://github.com/cybergaz/To_Do_List" target="_blank" rel="noreferrer">
                 <img src={githubIcon} alt="Visitar site" />
               </a> */}
-            </div>
+              </div>
             </header>
             <div className="body">
               <h3>Smart Genset</h3>
-              <p>An IoT based project implementing Generator System in industries using Iot,cloud and automation. 
+              <p>
+                An IoT based project implementing Generator System in industries
+                using Iot,cloud and automation.
               </p>
             </div>
             <footer>
@@ -132,37 +214,6 @@ export function Portfolio(){
             </footer>
           </div>
         </ScrollAnimation>
-
-        {/* <ScrollAnimation animateIn="flipInX">
-          <div className="project">
-            <header>
-            <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
-            <div className="project-links">
-              <a href="https://github.com/cybergaz/test_repo" target="_blank" rel="noreferrer">
-                <img src={githubIcon} alt="Visitar site" />
-              </a>
-              <a href="http://cybergaz.herokuapp.com" target="_blank" rel="noreferrer">
-                <img src={externalLinkIcon} alt="Visitar site" />
-              </a>
-            </div>
-            </header>
-            <div className="body">
-              <h3>Neet Coderz</h3>
-              <p>Just a simple webpage i built during my first year . just a nostalgic reminder for me : &#41;
-              </p>
-            </div>
-            <footer>
-              <ul className="tech-list">
-                <li>Html</li>
-                <li>Css</li>
-                <li>JavaSript</li>
-              </ul>
-            </footer>
-          </div>
-        </ScrollAnimation> */}
-
-
-
       </div>
     </Container>
   );
